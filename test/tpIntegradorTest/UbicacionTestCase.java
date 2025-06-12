6175a707ddf6ea36edac8e481829938a42ba08f5
Guido N Flamini <gflamini2000@gmail.com>
2025-06-12 18:38:30 -0300

    Implementados puntos 1 y 2 de Ubicacion

--- test/tpIntegradorTest/UbicacionTestCase.java
+++ test/tpIntegradorTest/UbicacionTestCase.java
@@ -3,11 +3,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import tpIntegrador.Ubicacion;
 
 public class UbicacionTestCase {
 	Ubicacion avellaneda = new Ubicacion(-34.6633, -58.3647);
 	Ubicacion quilmes = new Ubicacion(-34.7207, -58.2549);
+	Ubicacion madrid = new Ubicacion(40.4169, -3.7033);
 	
 	@BeforeEach
 	public void setUp() {
@@ -27,4 +32,12 @@ public class UbicacionTestCase {
 		assertEquals(avellaneda.distanciaEnKmA(quilmes), 11.895914535573418);
 	}
 	
+	@Test
+	void unaUbicacionPuedeConocerLasUbicacionesQueSeEncuentrenAMenosDeCiertaDistanciaAPartirDeUnaListaDeUbicaciones() {
+		List<Ubicacion> ubicaciones = Arrays.asList(quilmes, madrid);
+		List<Ubicacion> ubisCercanas = avellaneda.lasQueEstanAMenosDe_Km(ubicaciones, 20);
+		assertTrue(ubisCercanas.contains(quilmes));
+		assertFalse(ubisCercanas.contains(madrid));
+	}
+	 
 }
