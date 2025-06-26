ecdc6f2dbad8237412140357e4efdd81a8a964c5
Guido N Flamini <gflamini2000@gmail.com>
2025-06-26 01:46:16 -0300

    Algunos ajustes

--- src/tpIntegrador/GestorDeMuestras.java
+++ src/tpIntegrador/GestorDeMuestras.java
@@ -27,12 +27,17 @@ public class GestorDeMuestras {
 	}
 	
 	public void agregarMuestra(Muestra nuevaMuestra) {
-		nuevaMuestra.getUsuarioCreadorDeMuestra().enviarMuestra(nuevaMuestra);
-		muestras.add(nuevaMuestra);
-		List <ZonaDeCobertura>listaDeZonas = zonasDeCobertura.getZonas();
-		for(ZonaDeCobertura zona : listaDeZonas) {
-			zona.agregarMuestra(); // sólo va agregar la muestra si ésta se encuentra dentro de la zona
+		if(nuevaMuestra.laOpinionMasReciente().toString().startsWith("VINCHUCA")) {
+			nuevaMuestra.getUsuarioCreadorDeMuestra().enviarMuestra(nuevaMuestra);
+			muestras.add(nuevaMuestra);
+			List <ZonaDeCobertura>listaDeZonas = zonasDeCobertura.getZonas();
+			for(ZonaDeCobertura zona : listaDeZonas) {
+				zona.agregarMuestra(); // sólo va agregar la muestra si ésta se encuentra dentro de la zona
+			}
+		}else {
+			throw new IllegalArgumentException("No se puede subir una muestra cuya especie no sea vinchuca");
 		}
+		
 	}
 	
 	public List<Muestra> getMuestras(){
